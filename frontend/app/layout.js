c086c8e6e28c4961bbe6994ad02cb3410c45af6e
Abhishek Raj <abhishekbokaro442@gmail.com>
2025-08-26 12:19:33 +0530

    added visits

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,35 +1,42 @@
 import localFont from "next/font/local";
-import {Outfit} from "next/font/google"
+import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider, GoogleOneTap } from "@clerk/nextjs";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
 });
-const outfit = Outfit({subsets: ["latin"]});
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "CourseGPT",
-  description: "Generated Free AI courses",
+    title: "CourseGPT",
+    description: "Generated Free AI courses",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-      {/* <GoogleOneTap /> */}
-      
-        <body className={outfit.className}>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                {/* <GoogleOneTap /> */}
+
+                <body className={outfit.className}>
+                    {children}
+                    <div className="fixed bottom-4 right-4 z-50">
+                        <img
+                            src="https://visitor-badge.laobi.icu/badge?page_id=abhishekrj02.coursegpt&format=true&left_color=%23171717&right_color=%23141278"
+                            alt="visitor badge"
+                            className="w-auto h-auto"
+                        />
+                    </div>
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 }
